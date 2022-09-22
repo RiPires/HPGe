708950104bb28dd75b27132687ec51ad89d23da2
RiPires <fc49807@alunos.fc.ul.pt>
2022-09-22 13:00:52 +0200

    dev initialized

---
+++ HPGe_dev/build/CMakeFiles/HPGeDev.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HPGeDev.dir/HPGe_dev.cc.o"
+  "CMakeFiles/HPGeDev.dir/src/HPGeActionInitialization.cc.o"
+  "HPGeDev"
+  "HPGeDev.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HPGeDev.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
