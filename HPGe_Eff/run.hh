2273472193b6048596e519e8fefa573605e7d67a
RiPires-nucria <fc49807@alunos.fc.ul.pt>
2023-01-17 14:13:58 +0000

    Comments on actions, event, run and steppign

--- HPGe_Eff/run.hh
+++ HPGe_Eff/run.hh
@@ -15,3 +15,19 @@ public:
     virtual void EndOfRunAction(const G4Run*);
 };
 #endif
+
+///      G4UserRunAction.hh     ///
+///35 // class description:
+///36 //
+///37 //  This is the base class of a user's action class which defines the
+///38 // user's action at the begining and the end of each run. The user can
+///39 // override the following two methods but the user should not change 
+///40 // any of the contents of G4Run object.
+///41 //    virtual void BeginOfRunAction(const G4Run* aRun);
+///42 //    virtual void EndOfRunAction(const G4Run* aRun);
+///43 // The user can override the following method to instanciate his/her own
+///44 // concrete Run class. G4Run has a virtual method RecordEvent, so that
+///45 // the user can store any information useful to him/her with event statistics.
+///46 //    virtual G4Run* GenerateRun();
+///47 //  The user's concrete class derived from this class must be set to
+///48 // G4RunManager via G4RunManager::SetUserAction() method.
--- HPGe_Eff/stepping.hh
+++ HPGe_Eff/stepping.hh
@@ -20,3 +20,10 @@ private:
 };
 
 #endif
+
+
+///31 //  G4UserSteppingAction.hh
+///32 //
+///33 // class description:
+///34 //    This class represents actions taken place by the user at each
+///35 //    end of stepping. 
