a1daf2f5f446ea1065a313c35a2c58068f9f41de
RiPires-wsl <fc49807@alunos.fc.ul.pt>
2023-01-17 14:05:21 +0000

    Comments on actions

--- HPGe_Eff/action.cc
+++ HPGe_Eff/action.cc
@@ -28,3 +28,21 @@ void MyActionInitialization::Build() const
     MySteppingAction *steppingAction = new MySteppingAction(eventAction);
     SetUserAction(steppingAction);
 }   
+
+    // Id: G4VUserActionInitialization.hh 
+    // class description:
+    //
+    //  This is the abstract base class for instantiating all the user action classes.
+    // It has a pure virtual method Build() which is invoked by G4RunManager for
+    // sequential execution and G4WorkerRunManager for multi-threaded execution.
+    // The additional virtual method BuildForMaster() will be invoked from G4MTRunManager
+    // for multi-threaded execution.
+    //
+    //  Note that these virtual methods are const. It means the user may construct
+    // user action objects, but should not store the pointers of these objects as
+    // data members of the derived class.
+    //
+    // Note for multi-threaded mode:
+    //  The only action class the user may set to G4MTRunManager is a run action. It is
+    // then used at the beginning and the end of a run. It may be the same class or a
+    // dedicated class different from the run action instantiated for G4WorkerRunManager.
--- HPGe_Eff/mainSim.cc
+++ HPGe_Eff/mainSim.cc
@@ -14,35 +14,42 @@
 
 int main(int argc, char** argv)
 {
+    // Program starts with no user's interface
     G4UIExecutive *ui = 0;
 
+    // Checks for MultiThreading mode
     #ifdef G4MULTITHREADED
         G4MTRunManager * runManager = new G4MTRunManager();
     #else
         G4RunManager *runManager = new G4RunManager();
     #endif
-        
+    
+    // Initializes geometry Construction, Physics' lists and User Actions
     runManager->SetUserInitialization(new MyDetectorConstruction());
     runManager->SetUserInitialization(new MyPhysicsList());
     runManager->SetUserInitialization(new MyActionInitialization());
     
+    // Setting User's Interface on
     if(argc == 1)
     {
-        
         ui = new G4UIExecutive(argc, argv);
     }
     
+    // Initializes Visual Manager
     G4VisManager *visManager = new G4VisExecutive();
     visManager->Initialize();
     
+    // Pointer to Users' Interface manager ??
     G4UImanager *UImanager = G4UImanager::GetUIpointer();
     
+    // Checks for user's interface
+    // applies visual commands in "vis.mac" and starts session, if set on
     if(ui)
     {
         UImanager->ApplyCommand("/control/execute vis.mac");        
         ui->SessionStart();
     }
-    
+    // runs commands writen in a file "fileName", or runs command "fileName" from console, if set off
     else
     {
         G4String command = "/control/execute ";
