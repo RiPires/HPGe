17055e8c8787cd3f2d1d0faad1f7a2e93e0ef7bf
RiPires-nucria <fc49807@alunos.fc.ul.pt>
2023-01-19 11:43:03 +0000

    comments on event.cc and run.cc

--- HPGe_Eff/event.cc
+++ HPGe_Eff/event.cc
@@ -17,8 +17,11 @@ void MyEventAction::EndOfEventAction(const G4Event*)
 {
     ///G4cout << "Energy deposition: " << Edep << " MeV" << G4endl;
     
+    // Inicializes an instance of the AnalysisManager
     G4AnalysisManager *man = G4AnalysisManager::Instance();
     
+    // Fills tuple for energy deposition in the event
+    // and starts new row
     man->FillNtupleDColumn(2, 0, Edep);
     man->AddNtupleRow(2);
 }
--- HPGe_Eff/run.cc
+++ HPGe_Eff/run.cc
@@ -2,8 +2,10 @@
 
 MyRunAction::MyRunAction()
 {
+    // Inicializes an instance of the AnalysisManager
     G4AnalysisManager *man = G4AnalysisManager::Instance();
     
+    // Creates Tuple for Photons' info.
     man->CreateNtuple("Photons", "Photons");
     man->CreateNtupleIColumn("EventNr");
     man->CreateNtupleDColumn("EventX");
@@ -13,6 +15,7 @@ MyRunAction::MyRunAction()
     man->CreateNtupleDColumn("Photon_Energy");
     man->FinishNtuple(0);
     
+    // Creates Tuple for Detector's info.
     man->CreateNtuple("DetInfo", "DetInfo");
     man->CreateNtupleIColumn("EventNr");
     man->CreateNtupleDColumn("DetX");
@@ -20,6 +23,7 @@ MyRunAction::MyRunAction()
     man->CreateNtupleDColumn("DetZ");
     man->FinishNtuple(1); 
     
+    // Creates Tuple for Energy Scoring info.
     man->CreateNtuple("Scoring", "Scoring");
     man->CreateNtupleDColumn("Edep");
     man->FinishNtuple(2);
@@ -31,18 +35,26 @@ MyRunAction::~MyRunAction()
 
 void MyRunAction::BeginOfRunAction(const G4Run* run)
 {
+    // Inicializes an instance of the AnalysisManager
     G4AnalysisManager *man = G4AnalysisManager::Instance();
     
+    // Get Run Identification number as integer
     G4int runID = run->GetRunID();
+    // Turn it into string
     std::stringstream strRunID;
     strRunID << runID;
     
+    // Opens output ".root" file w/ Run identification number 
     man->OpenFile("output"+strRunID.str()+".root");
 }
 
 void MyRunAction::EndOfRunAction(const G4Run*)
-{
+{    
+    // Inicializes an instance of the AnalysisManager
     G4AnalysisManager *man = G4AnalysisManager::Instance();
+
+    // Writes data in the Tuples at the end of the run
+    // and closes the output file 
     man->Write();
     man->CloseFile();
 }
